e48fbb4329a49fab8156d03780da991d50e70ccd
Ho Huy Luat <hohuyluat1@gmail.com>
2022-10-24 09:09:54 +0700

    first commit

---
+++ 19110039/src/Components/DetailForm.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import axios from "axios";
+import CommentForm from "./CommentForm";
+
+
+const DetailForm = (props) => {
+
+  const [formValues, setFormValues] = 
+    useState({comment: ''})
+ 
+  const { id, content, comment}=props.initialValues;
+
+// let listComment={};
+//   for(let value of comment){
+//     listComment.push(value);
+//   }
+//const comt= JSON.stringify(comment);
+
+  // console.log('comment', comment);
+    const onSubmit = blogObject => {
+      axios.post(
+  'http://localhost:5000/blog/comment/'+id, 
+      blogObject)
+        .then(res => {
+          if (res.status === 200){
+            alert('Blog successfully created');
+            window.location = "/detail-blog/"+id;
+            setFormValues();
+          }
+          else
+            Promise.reject()
+        })
+        .catch(err => alert('Something went wrong'))
+    }
+      
+
+    
+  // let abc= {conts}.content;
+//var abc= JSON.parse({content});
+//const conts= {content}.content;
+  return (
+    <div>
+      <h2>BLOG HỌC TẬP</h2>
+        <section>
+          <article class="detailcontainer">
+            <h2>{content}</h2>
+          </article>
+        </section>
+        <CommentForm initialValues={formValues} 
+              onSubmit={onSubmit} 
+              enableReinitialize>
+              Comment
+        </CommentForm>
+         <div>{comment}</div>
+    </div>
+    
+  );
+};
+  
+export default DetailForm;
